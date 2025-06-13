d7972777eb8d82ce80ddb5b6a6313f2c1367f112
Efren <curiefren78@gmail.com>
2025-06-13 02:32:03 -0600

    Implementacion de manejo de excepciones y paso de parametros

--- main.py
+++ main.py
@@ -3,31 +3,52 @@ from control import Simulacion #importa la clase Simulacion
 def mostrar_menu(): #define una funcion para mostrar el menu 
 
     print(" Menu :D ")
+    print("----------------------------------")
     print("1. Iniciar simulacion")
     print("2. Detener Simulacion")
     print("3. Reiniciar simulacion")
     print("4. Mostrar estado del trafico")
     print("5. Salir")
 
-def main():
-    sim = Simulacion() #crea una instancia de la clase
+sim = Simulacion() #crea una instancia de la clase
 
-    while True:
-        mostrar_menu()
-        op = input("Opcion")
-        # segun la opcion elegida, llama al metodo correspondiente
-        if op == "1":
-            sim.start()
-        elif op == "2":
-            sim.stop()
-        elif op == "3":
-            sim.reload()
-        elif op == "4":
-            sim.estado()
-        elif op == "5":
-            sim.stop()
-            print("Cerrando el programa :D")
-            break
-        else:
-            print("Opcion invalida")
+
+while True:
+    mostrar_menu()
+    op = input("Ingrese su opcion: ")
+
+    try:
+        op = int(op)
+    except ValueError:
+        print("Debes ingresar un numero.")
+        print("__________________________")
+        continue
+
+    match op:
+        case 1:
+            try:
+                p = int(input("Ingrese el número de pasos que durará la simulación: "))
+                d = float(input("Ingrese el delay para cada paso (en segundos): "))
+            except ValueError:
+                print("Debes ingresar números válidos.")
+                continue
             
+            sim.start(p, d)
+        case 2:
+            sim.stop()
+        case 3:
+            try:
+                p = int(input("Ingrese el número de pasos que durará la simulación: "))
+                d = float(input("Ingrese el delay para cada paso (en segundos): "))
+            except ValueError:
+                print("Debes ingresar números válidos.")
+                continue
+
+            sim.reload(p, d)
+        case 4:
+            sim.estado()
+        case 5: 
+            sim.stop()
+            print("Saliendo...")
+        case _:
+            print("Opcion no valida")
